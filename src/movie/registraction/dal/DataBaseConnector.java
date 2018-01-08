2e7986d9b54e425604245ef2184e7e011b79909e
davidmsf <davidmsf@gmail.com>
2018-01-08 10:44:09 +0100

    DatabaseConnector

---
+++ src/movie/registraction/dal/DataBaseConnector.java
@@ -0,0 +1,42 @@
+package movie.registraction.dal;
+
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+import java.sql.Connection;
+
+/**
+ *
+ * @author Hussain
+ */
+public class DataBaseConnector
+{
+
+    private final SQLServerDataSource dataSource;
+
+    /**
+     * Setting the relevant info to the database connection.
+     */
+    public DataBaseConnector()
+    {
+        dataSource = new SQLServerDataSource();
+
+        dataSource.setServerName("EASV-DB2");
+        dataSource.setPortNumber(1433);
+        dataSource.setDatabaseName("Movie_Collection");
+        dataSource.setUser("CS2017A_8_java");
+        dataSource.setPassword("javajava");
+    }
+
+    /**
+     * Getting the connection from the database.
+     *
+     * @return
+     *
+     * @throws SQLServerException
+     */
+    public Connection getConnection() throws SQLServerException
+    {
+        return dataSource.getConnection();
+    }
+
+}
