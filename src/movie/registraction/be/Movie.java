6cbca81b9d780cb726a4520a2c56f05f7a1d5759
davidmsf <davidmsf@gmail.com>
2018-01-08 09:59:15 +0100

    Added movie class

---
+++ src/movie/registraction/be/Movie.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package movie.registraction.be;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author B
+ */
+public class Movie {
+    
+    List<String> categories = new ArrayList();
+
+    public List<String> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<String> categories) {
+        this.categories = categories;
+    }
+}
