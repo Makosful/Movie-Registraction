853ad2d0087ef34f99829cf2e719795b57d005f9
dankphetamine <asge0907@easv365.dk>
2018-01-11 11:11:08 +0100

    Refactor file filter

--- src/movie/registraction/gui/controller/MainWindowController.java
+++ src/movie/registraction/gui/controller/MainWindowController.java
@@ -30,8 +30,7 @@ import movie.registraction.gui.model.MainWindowModel;
  *
  * @author Axl
  */
-public class MainWindowController implements Initializable
-{
+public class MainWindowController implements Initializable {
 
     //<editor-fold defaultstate="collapsed" desc="FXML Variables">
     @FXML
@@ -83,8 +82,7 @@ public class MainWindowController implements Initializable
      * @param rb
      */
     @Override
-    public void initialize(URL url, ResourceBundle rb)
-    {
+    public void initialize(URL url, ResourceBundle rb) {
         bindTileToScroll();
 
         // Access the Model
@@ -100,32 +98,27 @@ public class MainWindowController implements Initializable
         comboBoxSetup();
     }
 
-    private void comboBoxSetup()
-    {
+    private void comboBoxSetup() {
         comBoxSortOrder.getItems().addAll("Ascending", "Descending");
         comBoxMinRating.getItems().addAll("min. 1 star", "min. 2 stars", "min. 3 stars", "min. 4 stars", "min. 5 stars", "min. 6 stars", "min. 7 stars", "min. 8 stars", "min. 9 stars");
     }
 
-    private void modalWindowSetup()
-    {
+    private void modalWindowSetup() {
         //TODO
     }
 
     @FXML
-    private void titleSearch(ActionEvent event)
-    {
+    private void titleSearch(ActionEvent event) {
         model.fxmlTitleSearch(txtTitleSearch.getText());
     }
 
     @FXML
-    private void clearFilters(ActionEvent event)
-    {
+    private void clearFilters(ActionEvent event) {
         model.fxmlClearFilters();
     }
 
     @FXML
-    private void btnChangeCategories(ActionEvent event) throws IOException
-    {
+    private void btnChangeCategories(ActionEvent event) throws IOException {
         File fxml = new File("src/movie/registraction/gui/view/editCategories.fxml");
         FXMLLoader fxmlLoader = new FXMLLoader(fxml.toURL());
         Parent root;
@@ -142,8 +135,7 @@ public class MainWindowController implements Initializable
     }
 
     @FXML
-    private void btnChangeMovieCategory(ActionEvent event) throws MalformedURLException, IOException
-    {
+    private void btnChangeMovieCategory(ActionEvent event) throws MalformedURLException, IOException {
 
         File fxml = new File("src/movie/registraction/gui/view/EditMovieCategory.fxml");
         FXMLLoader fxmlLoader = new FXMLLoader(fxml.toURL());
@@ -161,87 +153,71 @@ public class MainWindowController implements Initializable
     }
 
     @FXML
-    private void uploadFiles(ActionEvent event)
-    {
+    private void uploadFiles(ActionEvent event) {
         // model.fxmlUploadFiles();
         setPictures(); // Midlertidigt.
     }
 
-    private void setPictures()
-    {
+    private void setPictures() {
         // Creates a new FileChooser object
         FileChooser fc = new FileChooser();
 
         // Defines what files it will look for
-        FileChooser.ExtensionFilter mp4Filter = new FileChooser.ExtensionFilter("MP4 Files", "*.mp4");
-        FileChooser.ExtensionFilter mpeg4Filter = new FileChooser.ExtensionFilter("MPEG4 Files", "*.mpeg4");
-        FileChooser.ExtensionFilter imgFilter = new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg");
+        FileChooser.ExtensionFilter videoFilter = new FileChooser.ExtensionFilter("MP4 Files", "*.mp4", ".mpeg4");
+        FileChooser.ExtensionFilter imageFilter = new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg");
 
         // Adds the filters
-        fc.getExtensionFilters().addAll(mp4Filter, mpeg4Filter, imgFilter);
+        fc.getExtensionFilters().addAll(videoFilter, imageFilter);
 
         // Opens the FileChooser and saves the results in a list
         List<File> chosenFiles = fc.showOpenMultipleDialog(null);
 
         // Checks if any files where chosen
-        if (chosenFiles != null)
-        {
+        if (chosenFiles != null) {
             // If valid files were chosen, add them as movies
             //List<File> addedFiles;
             model.setPictures(tilePane, chosenFiles);
             imageClick(tilePane, model.getContextMenu());
-        }
-            
-        else
-        {
+        } else {
             // Otherwise return
             System.out.println("One or more invalid file(s) / None selected");
             return;
         }
-        }
+    }
 
     /*
      * Binds the TilePane to the ScrollPane, height n width.
      */
-    private void bindTileToScroll()
-    {
+    private void bindTileToScroll() {
         tilePane.prefWidthProperty().bind(scrlFilterSearch.widthProperty());
         tilePane.prefHeightProperty().bind(scrlFilterSearch.heightProperty());
     }
-    
-        /*
+
+    /*
     Code so you can click or right click on an image and soemthing happens.
     Mouse event.
-    */  
-    private void imageClick(TilePane tilePane, ContextMenu contextMenu)
-    {
-        for(ImageView imageView : model.GetImageViewList())
-        {
-        imageView.setOnMouseClicked(new EventHandler<MouseEvent>() 
-        {
-            @Override
-            public void handle(MouseEvent event) 
-            {
-                MouseButton mouseButton = event.getButton();
-                if (mouseButton == MouseButton.PRIMARY) 
-                {
-                    model.closeMenuOrClick(contextMenu);
-                }
+     */
+    private void imageClick(TilePane tilePane, ContextMenu contextMenu) {
+        for (ImageView imageView : model.GetImageViewList()) {
+            imageView.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    MouseButton mouseButton = event.getButton();
+                    if (mouseButton == MouseButton.PRIMARY) {
+                        model.closeMenuOrClick(contextMenu);
+                    }
 
-                if (mouseButton == MouseButton.SECONDARY) 
-                {
-                    model.contextMenuOpenOrNot(contextMenu);
-                    contextMenu.show(tilePane, event.getScreenX(), event.getScreenY());
+                    if (mouseButton == MouseButton.SECONDARY) {
+                        model.contextMenuOpenOrNot(contextMenu);
+                        contextMenu.show(tilePane, event.getScreenX(), event.getScreenY());
+                    }
                 }
-            }
-
-        });
+            });
         }
     }
 
     @FXML
-    private void setLibrary(ActionEvent event)
-    {
+    private void setLibrary(ActionEvent event) {
         model.fxmlSetLibrary();
     }
 }
