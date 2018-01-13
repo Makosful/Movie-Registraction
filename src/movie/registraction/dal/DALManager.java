a7fbe8432b1177215aa181d8af251536eecbdc31
davidmsf <davidmsf@gmail.com>
2018-01-13 14:22:46 +0100

    removed unused class

--- src/movie/registraction/dal/DALManager.java
+++ src/movie/registraction/dal/DALManager.java
@@ -4,7 +4,6 @@ import java.io.*;
 import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import javafx.collections.ObservableList;
 import movie.registraction.be.Movie;
