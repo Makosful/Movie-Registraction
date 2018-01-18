87a83f9c352dfd1ccd9c7567bb2f5f7d56f25ab5
Axl <makosful@gmail.com>
2018-01-18 13:17:51 +0100

    Added comments to LibraryScan

--- src/movie/registraction/dal/LibraryScan.java
+++ src/movie/registraction/dal/LibraryScan.java
@@ -3,8 +3,10 @@ package movie.registraction.dal;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
@@ -15,27 +17,21 @@ import javafx.collections.ObservableList;
 public class LibraryScan implements Runnable
 {
 
-    private Path path;
-    private ObservableList<Path> list;
+    private final ObservableList<Path> list;
+
     private List folders;
 
+    /**
+     * Constructor
+     */
     public LibraryScan()
     {
         this.list = FXCollections.observableArrayList();
     }
 
-    public LibraryScan(Path root)
-    {
-        this.path = root;
-        this.list = FXCollections.observableArrayList();
-    }
-
-    public LibraryScan(ArrayList list)
-    {
-        this.folders = list;
-        this.list = FXCollections.observableArrayList();
-    }
-
+    /**
+     * Implimented from Runnable
+     */
     @Override
     public void run()
     {
@@ -43,6 +39,11 @@ public class LibraryScan implements Runnable
         //watch(path);
     }
 
+    /**
+     * Sets a WatchService on all Paths provided
+     *
+     * @param folders A List of Paths to watch
+     */
     private void watch(List<Path> folders)
     {
         try (WatchService watcher = FileSystems.getDefault().newWatchService())
@@ -95,72 +96,21 @@ public class LibraryScan implements Runnable
         }
     }
 
-    private void watch(Path path)
-    {
-        try (WatchService watcher = FileSystems.getDefault().newWatchService())
-        {
-            Map<WatchKey, Path> keyMap = new HashMap<>();
-
-            Files.walkFileTree(
-                    path, new SimpleFileVisitor<Path>()
-            {
-                @Override
-                public FileVisitResult preVisitDirectory(
-                        Path dir,
-                        BasicFileAttributes attrs)
-                        throws IOException
-                {
-                    keyMap.put(dir.register(watcher,
-                                            StandardWatchEventKinds.ENTRY_CREATE,
-                                            StandardWatchEventKinds.ENTRY_DELETE,
-                                            StandardWatchEventKinds.ENTRY_MODIFY),
-                               dir);
-                    return FileVisitResult.CONTINUE;
-                }
-            });
-
-            Collection<Path> values = keyMap.values();
-
-            WatchKey key;
-
-            do
-            {
-                key = watcher.take();
-                Path eventDir = keyMap.get(key);
-
-                for (WatchEvent<?> event : key.pollEvents())
-                {
-                    WatchEvent.Kind<?> kind = event.kind();
-                    Path eventPath = (Path) event.context();
-
-                    if ("ENTRY_DELETE".equals(kind.name()))
-                    {
-                        Path toPath = new File(eventDir + "/" + eventPath).toPath();
-                        // System.out.println("Deleted");
-                        this.list.add(toPath);
-                    }
-                    if ("ENTRY_CREATE".equals(kind.name()))
-                    {
-                        Path toPath = new File(eventDir + "/" + eventPath).toPath();
-                        // System.out.println("Created");
-                        this.list.add(toPath);
-                    }
-
-                    //System.out.println(eventDir + ": " + kind + ": " + eventPath);
-                }
-            }
-            while (key.reset());
-        }
-        catch (IOException | InterruptedException ex)
-        {
-        }
-    }
-
+    /**
+     * Gets the list containing the changes
+     *
+     * @return Returns an ObservableList containing all the changed files' paths
+     */
     public ObservableList<Path> getObsList()
     {
         return this.list;
     }
 
+    /**
+     * Sets the folders' list to reference the given list
+     *
+     * @param folderList The list to mirror
+     */
     public void setFolders(ArrayList<Path> folderList)
     {
         this.folders = folderList;
