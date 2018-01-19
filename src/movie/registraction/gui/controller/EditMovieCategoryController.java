1031d989b8d6bcfdc6a18d4e5ff255198cbf2e9a
davidmsf <davidmsf@gmail.com>
2018-01-19 09:52:50 +0100

    removed import

--- src/movie/registraction/gui/controller/EditMovieCategoryController.java
+++ src/movie/registraction/gui/controller/EditMovieCategoryController.java
@@ -3,7 +3,6 @@ package movie.registraction.gui.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.beans.value.ObservableValue;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
