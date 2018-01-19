e813acda350f47e0136a5df767429dba54254507
davidmsf <davidmsf@gmail.com>
2018-01-19 08:45:47 +0100

    documentation

--- src/movie/registraction/gui/controller/EditCategoriesController.java
+++ src/movie/registraction/gui/controller/EditCategoriesController.java
@@ -48,18 +48,30 @@ public class EditCategoriesController implements Initializable
         lstViewAllCategories.setItems(m.loadCategories());
     }
 
+    /**
+     * Add a specific category, by passing string as category  
+     * @param event 
+     */
     @FXML
     private void btnAddCategory(ActionEvent event)
     {
         m.addChosenCategory(txtFieldCategory.getText());
     }
 
+    /**
+     * Remove a specific category, by passing string as category  
+     * @param event 
+     */
     @FXML
     private void btnRemoveCategory(ActionEvent event)
     {
         m.removeChosenCategory(lstViewAllCategories.getSelectionModel().getSelectedItem());
     }
 
+    /**
+     * Make a call down to save the changes and then closes the window
+     * @param event 
+     */
     @FXML
     private void btnSaveCategories(ActionEvent event)
     {
